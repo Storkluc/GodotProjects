fc9bb95ceb23f7c545e8dd40cd14437aadee98a2
Lucas Oliveira <42073344+Storkluc@users.noreply.github.com>
2019-05-26 15:16:31 -0300

    Second
    Finalizations, but I need to change some things yet.

---
+++ FelpudoFly/scripts/Cano.gd
@@ -0,0 +1,24 @@
+extends Node2D
+export var vel = -600
+var cena
+
+func _ready():
+	cena = get_tree().get_current_scene()
+	set_process(true)
+	
+func _process(delta):
+	if cena.estado == cena.JOGANDO:
+		set_pos(get_pos() + Vector2(vel * delta, 0))
+	
+	if get_pos().x <-280:
+		print("Destruido")
+		queue_free()
+
+
+func _on_Area2D_body_enter( body ):
+	if body.get_name() == "Felpudo":
+		cena.kill()
+
+
+func _on_Ponto_body_enter( body ):
+	cena.pontuar()
